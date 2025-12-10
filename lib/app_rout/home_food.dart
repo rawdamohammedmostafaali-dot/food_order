2c17a9ad56a7ed3cd2a626afe9c229a327d8b563
rawda <rawdamohammedmostafaali@gmail.com>
2025-12-10 13:18:41 +0200

    success

--- lib/app_rout/home_food.dart
+++ lib/app_rout/home_food.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:food_order/feature/home/presentation/ui/home_screen_food_order.dart';
 
 class HomeFood extends StatelessWidget {
   const HomeFood({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreenFoodOrder(),
+    );
   }
 }
